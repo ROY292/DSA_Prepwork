b02f434c97f440d506477cbd01f59e20e6253387
Birat Roy <40495257+ROY292@users.noreply.github.com>
2025-08-25 00:19:45 +0530

    Time: 4 ms (17.05%) | Memory: 65.8 MB (5.09%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,25 +1,68 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        // Number of zero's in the window.
-        int zeroCount = 0;
-        int longestWindow = 0;
-        // Left end of the window.
-        int start = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            zeroCount += (nums[i] == 0);
-                          
-            // Shrink the window until the count of zero's
-            // is less than or equal to 1.
-            while (zeroCount > 1) {
-                zeroCount -= (nums[start] == 0);
-                start++;
+
+        if(nums.size()== 1) return 0;
+
+        vector<int> left(nums.size(),0),right(nums.size(),0);
+
+        left[0] = nums[0];
+        right[nums.size()-1] = nums[nums.size()-1];
+
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(nums[i] == 0)
+            {
+                left[i] = 0;
             }
-              
-            longestWindow = max(longestWindow, i - start);
+            else
+            {
+                left[i] = left[i-1] + nums[i];
+            }
         }
 
-        return longestWindow;
+        for(int i = nums.size()-2;i>=0;i--)
+        {
+            if(nums[i] == 0)
+            {
+                right[i] = 0;
+            }
+            else
+            {
+                right[i] = right[i+1] + nums[i];
+            }
+        }
+        if(left[nums.size()-1] == nums.size())
+            return nums.size()-1;
+        if(nums[0] == 0 && right[1] == nums.size() - 1)
+            return nums.size()-1;
+        
+        if(nums[nums.size()-1] == 0 && left[nums.size()-2] == nums.size()-1)
+        return nums.size()-1;
+
+        int ans = 0;
+        // for(auto x: left)
+        //     cout<<" left: "<<x<<endl;
+        
+        // for(auto x: right)
+        //     cout<<" right: "<<x<<endl;
+
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i] == 0)
+            {
+                if(i == 0)
+                    ans = max(ans, right[i+1]);
+                else if(i == nums.size()-1)
+                    ans = max(ans, left[i-1]);
+                else
+                    ans = max(ans, (left[i-1] + right[i+1]));
+            }
+        }
+
+        return ans;
+
+        
     }
+
 };
