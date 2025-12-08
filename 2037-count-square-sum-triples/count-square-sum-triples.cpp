5989cd84d9969b654bc68bcf89c7e4017a608a6b
Birat Roy <40495257+ROY292@users.noreply.github.com>
2025-12-08 22:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (84.51%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,19 +1,35 @@
+// class Solution {
+// public:
+//     int countTriples(int n) {
+//         int cnt = 0;
+
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j = 1;j<=n;j++)
+//             {
+//                 int temp = (i*i) + (j*j);
+//                 int t = (int)sqrt(temp);
+//                 if(t*t == temp && t <= n)
+//                     cnt++;
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int countTriples(int n) {
-        int cnt = 0;
-
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j = 1;j<=n;j++)
-            {
-                int temp = (i*i) + (j*j);
-                int t = (int)sqrt(temp);
-                if(t*t == temp && t <= n)
-                    cnt++;
+        int res = 0;
+        for (int u = 2; u <= sqrt(n); u++) {
+            for (int v = 1; v < u; v++) {
+                if (~(u - v) & 1 || gcd(u, v) != 1) continue;
+                int c = u * u + v * v;
+                if (c > n) continue;
+                res += (n / c) << 1;
             }
         }
-
-        return cnt;
+        return res;
     }
 };
