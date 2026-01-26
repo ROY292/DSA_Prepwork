e846efd4ceedca70cd4b67e9fc2f266ddd5ce9bd
Birat Roy <40495257+ROY292@users.noreply.github.com>
2026-01-26 22:27:45 +0530

    Time: 15 ms (76.77%) | Memory: 36.7 MB (32.02%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
+/* Think, if you run n2 for finding minimums is it worth it. like the minimuim difference will always be with the closest possible value. so using that fact we can combine both the passes into one. we know that if we encounter a new min difference, that means it was not before so all the pairs before this pair that we thought is minimum is not actually minimum. And it starts from my current pair. Remember only the closest value will give the minimum pair so no point comparing with every value in sorted array. */
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
 
         sort(arr.begin(),arr.end());
         int difference = INT_MAX;
+        vector<vector<int>> ans;
+
+        // for(int i = 1;i<arr.size();i++)
+        // {
+        //     difference = min(difference,abs(arr[i] - arr[i-1]));
+        // }
+       
+        // for(int i = 0;i<arr.size();i++)
+        // {
+        //     for(int j = i+1;j<arr.size();j++)
+        //     {
+        //         if(abs(arr[i] - arr[j]) == difference)
+        //         {
+        //             ans.push_back({arr[i],arr[j]});
+        //         }
+        //         if(abs(arr[i] - arr[j]) > difference)
+        //             break;
+        //     }
+        // }
 
         for(int i = 1;i<arr.size();i++)
         {
-            difference = min(difference,abs(arr[i] - arr[i-1]));
-        }
-        vector<vector<int>> ans;
-        for(int i = 0;i<arr.size();i++)
-        {
-            for(int j = i+1;j<arr.size();j++)
+            int mindif = abs(arr[i] - arr[i-1]);
+            if(mindif < difference)
             {
-                if(abs(arr[i] - arr[j]) == difference)
-                {
-                    ans.push_back({arr[i],arr[j]});
-                }
-                if(abs(arr[i] - arr[j]) > difference)
-                    break;
+                ans.clear();
+                ans.push_back({arr[i-1],arr[i]});
+                difference = mindif;
+            }
+            else if( mindif == difference)
+            {
+                ans.push_back({arr[i-1],arr[i]});
             }
         }
         return ans;
